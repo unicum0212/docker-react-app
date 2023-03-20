95913950d761338d0591265d153d2a5968748e70
Dima Tsykalenko <106827785+unicum0212@users.noreply.github.com>
2023-03-20 17:21:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Feature branch
+          Learn React
         </a>
       </header>
     </div>
